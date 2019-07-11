b900c37e71e9e84b27fcc9a37f310048051f9ca3
John Cook <johncookds@gmail.com>
2019-07-11 08:35:38 -0700

    Update relations/routes/api.py
    Co-Authored-By: Erick <brp53@cornell.edu>

--- relations/routes/api.py
+++ relations/routes/api.py
@@ -42,7 +42,7 @@ def supercede(arxiv_id_str: str, arxiv_ver: int, relation_id_str: str) -> Respon
 
 @blueprint.route('/<string:arxiv_id_str>v<int:arxiv_ver>/relations/<string:relation_id_str>', methods=['DELETE'])
 def suppress(arxiv_id_str: str, arxiv_ver: int, relation_id_str: str) -> Response:
-    """create a new relation for an e-print which supresses an existing relation"""
+    """Create a new relation for an e-print which supresses an existing relation."""
     response_data, status_code, headers = controllers.suppress(arxiv_id_str, arxiv_ver, relation_id_str, request.data)
     response: Response = jsonify(response_data)
     response.status_code = status_code
