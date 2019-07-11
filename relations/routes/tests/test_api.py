d4f72abe73004ec8de181264eb4e05bcd9420bf7
johncookds <johncook@Johns-MacBook-Pro.local>
2019-07-10 20:34:54 -0700

    test apis'

--- relations/routes/tests/test_api.py
+++ relations/routes/tests/test_api.py
@@ -33,7 +33,7 @@ class TestExternalAPIRoutes(TestCase):
                     'resource_id':1012,
                     'description':'test relation',
                     'creator':'tester'}
-        return_data={'mock_ID':{'relation_values':'vals'}}
+        return_data={'relation_values':'vals'}
         mock_create_new.return_value = return_data, HTTPStatus.OK, {}
         token = generate_token('1234', 'foo@user.com', 'foouser',
                                scope=[])
@@ -42,7 +42,7 @@ class TestExternalAPIRoutes(TestCase):
                                     headers={'Authorization':token},
                                     content_type='application/json')
 
-        expected_data = {'mock_ID':return_data['mock_ID']}
+        expected_data = return_data
 
         self.assertEqual(response.status_code, HTTPStatus.OK)
         self.assertDictEqual(json.loads(response.data), expected_data)
@@ -55,7 +55,7 @@ class TestExternalAPIRoutes(TestCase):
                     'resource_id':1012,
                     'description':'test relation',
                     'creator':'tester'}
-        return_data={'mock_ID':{'relation_values':'vals'}}
+        return_data={'relation_values':'vals'}
         mock_supercede.return_value = return_data, HTTPStatus.OK, {}
         token = generate_token('1234', 'foo@user.com', 'foouser',
                                scope=[])
@@ -64,7 +64,7 @@ class TestExternalAPIRoutes(TestCase):
                                     headers={'Authorization':token},
                                     content_type='application/json')
 
-        expected_data = {'mock_ID':return_data['mock_ID']}
+        expected_data = return_data
 
         self.assertEqual(response.status_code, HTTPStatus.OK)
         self.assertDictEqual(json.loads(response.data), expected_data)
@@ -77,7 +77,7 @@ class TestExternalAPIRoutes(TestCase):
                     'resource_id':1012,
                     'description':'test relation',
                     'creator':'tester'}
-        return_data={'mock_ID':{'relation_values':'vals'}}
+        return_data={'relation_values':'vals'}
         mock_suppress.return_value = return_data, HTTPStatus.OK, {}
         token = generate_token('1234', 'foo@user.com', 'foouser',
                                scope=[])
@@ -86,7 +86,7 @@ class TestExternalAPIRoutes(TestCase):
                                     headers={'Authorization':token},
                                     content_type='application/json')
 
-        expected_data = {'mock_ID':return_data['mock_ID']}
+        expected_data = return_data
 
         self.assertEqual(response.status_code, HTTPStatus.OK)
         self.assertDictEqual(json.loads(response.data), expected_data)
@@ -96,13 +96,13 @@ class TestExternalAPIRoutes(TestCase):
     def test_get_relations(self, mock_get_relations: Any) -> None:
         """Endpoint /api/<string:arxiv_id_str>v<int:arxiv_ver> returns JSON about all relations belonging to an id."""
 
-        return_data = {'mock_ID':{'relation1':{'description':''}}}
+        return_data = {'relation1':{'description':''},'relation2':{'description':''}}
         mock_get_relations.return_value = return_data, HTTPStatus.OK, {}
         token = generate_token('1234', 'foo@user.com', 'foouser',
                                scope=[])
         response = self.client.get('/api/TESTv1',headers={'Authorization':token})
 
-        expected_data = {'mock_ID': return_data['mock_ID']}
+        expected_data = return_data
 
         self.assertEqual(response.status_code, HTTPStatus.OK)
         self.assertDictEqual(json.loads(response.data), expected_data)
@@ -112,13 +112,13 @@ class TestExternalAPIRoutes(TestCase):
     def test_get_events(self, mock_get_events: Any) -> None:
         """Endpoint /api/<string:arxiv_id_str>v<int:arxiv_ver>/log returns JSON about all events belonging to an id."""
 
-        return_data = {'mock_ID':{'event1':{'description':''}}}
+        return_data = {'event1':{'description':''},'event1':{'description':''}}
         mock_get_relations.return_value = return_data, HTTPStatus.OK, {}
         token = generate_token('1234', 'foo@user.com', 'foouser',
                                scope=[])
         response = self.client.get('/api/TESTv1/log',headers={'Authorization':token})
 
-        expected_data = {'mock_ID': return_data['mock_ID']}
+        expected_data = return_data
 
         self.assertEqual(response.status_code, HTTPStatus.OK)
         self.assertDictEqual(json.loads(response.data), expected_data)
