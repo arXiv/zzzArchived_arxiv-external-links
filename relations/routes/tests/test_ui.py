08a72f4ebf3f1f515453908a5c16be5c5a7b5030
Takeo Imai <takeo.bono@gmail.com>
2019-08-06 06:38:37 +0900

    follow pep8

--- relations/routes/tests/test_ui.py
+++ relations/routes/tests/test_ui.py
@@ -16,6 +16,7 @@ from arxiv.users.domain import Scope
 from relations.factory import create_ui_app
 from .. import ui
 
+
 class TestUIRoutes(TestCase):
     """tests for UI routes."""
 
@@ -28,21 +29,20 @@ class TestUIRoutes(TestCase):
     @mock.patch(f'{ui.__name__}.controllers.create_new')
     def test_create_new(self, mock_create_new: Any) -> None:
         """Endpoint /<string:arxiv_id_str>v<int:arxiv_ver>/relations returns html about the newly created relation."""
-
-        post_data = {'resource_type':'type',
-                    'resource_id':1012,
-                    'description':'test relation',
-                    'creator':'tester'}
-        return_data={'arxiv_id':'TEST',
-                    'arxiv_ver':10,
-                    'resource_type':'type',
-                    'description':'test relation'}
+        post_data = {'resource_type': 'type',
+                     'resource_id': 1012,
+                     'description': 'test relation',
+                     'creator': 'tester'}
+        return_data = {'arxiv_id': 'TEST',
+                       'arxiv_ver': 10,
+                       'resource_type': 'type',
+                       'description': 'test relation'}
         mock_create_new.return_value = return_data, HTTPStatus.OK, {}
         token = generate_token('1234', 'foo@user.com', 'foouser',
                                scope=[])
         response = self.client.post('/TESTv10/relations',
                                     data=json.dumps(post_data),
-                                    headers={'Authorization':token},
+                                    headers={'Authorization': token},
                                     content_type='application/json')
 
         self.assertEqual(response.status_code, HTTPStatus.OK)
@@ -52,22 +52,21 @@ class TestUIRoutes(TestCase):
     @mock.patch(f'{ui.__name__}.controllers.supercede')
     def test_supercede(self, mock_supercede: Any) -> None:
         """Endpoint /<string:arxiv_id_str>v<int:arxiv_ver>/relations/<string:relation_id_str> returns html about the newly created relation and the relation it superceded."""
-
-        post_data = {'resource_type':'type',
-                    'resource_id':1012,
-                    'description':'test relation',
-                    'creator':'tester'}
-        return_data={'arxiv_id':'TEST',
-                    'arxiv_ver':10,
-                    'relation_id':1012,
-                    'resource_type':'type',
-                    'description':'test relation'}        
+        post_data = {'resource_type': 'type',
+                     'resource_id': 1012,
+                     'description': 'test relation',
+                     'creator': 'tester'}
+        return_data = {'arxiv_id': 'TEST',
+                       'arxiv_ver': 10,
+                       'relation_id': 1012,
+                       'resource_type': 'type',
+                       'description': 'test relation'}
         mock_supercede.return_value = return_data, HTTPStatus.OK, {}
         token = generate_token('1234', 'foo@user.com', 'foouser',
                                scope=[])
         response = self.client.post('/TESTv10/relations/relation1',
                                     data=json.dumps(post_data),
-                                    headers={'Authorization':token},
+                                    headers={'Authorization': token},
                                     content_type='application/json')
 
         self.assertEqual(response.status_code, HTTPStatus.OK)
@@ -77,22 +76,21 @@ class TestUIRoutes(TestCase):
     @mock.patch(f'{ui.__name__}.controllers.suppress')
     def test_suppress(self, mock_suppress: Any) -> None:
         """Endpoint /<string:arxiv_id_str>v<int:arxiv_ver>/relations/<string:relation_id_str>/delete returns html about the newly created relation and the relation it suppressed."""
-
-        post_data = {'resource_type':'type',
-                    'resource_id':1012,
-                    'description':'test relation',
-                    'creator':'tester'}
-        return_data={'arxiv_id':'TEST',
-                    'arxiv_ver':10,
-                    'relation_id':1012,
-                    'resource_type':'type',
-                    'description':'test relation'}
+        post_data = {'resource_type': 'type',
+                     'resource_id': 1012,
+                     'description': 'test relation',
+                     'creator': 'tester'}
+        return_data = {'arxiv_id': 'TEST',
+                       'arxiv_ver': 10,
+                       'relation_id': 1012,
+                       'resource_type': 'type',
+                       'description': 'test relation'}
         mock_suppress.return_value = return_data, HTTPStatus.OK, {}
         token = generate_token('1234', 'foo@user.com', 'foouser',
                                scope=[])
         response = self.client.post('/TESTv10/relations/relation1/delete',
                                     data=json.dumps(post_data),
-                                    headers={'Authorization':token},
+                                    headers={'Authorization': token},
                                     content_type='application/json')
 
         expected_data = return_data
@@ -104,12 +102,11 @@ class TestUIRoutes(TestCase):
     @mock.patch(f'{ui.__name__}.controllers.retrieve')
     def test_get_relations(self, mock_retrieve: Any) -> None:
         """Endpoint /<string:arxiv_id_str>v<int:arxiv_ver> returns html about all relations belonging to an id."""
-
-        return_data={'relations':[{'key1':'val1'},{'key1':'val2'}]}  
+        return_data = {'relations': [{'key1': 'val1'}, {'key1': 'val2'}]}
         mock_retrieve.return_value = return_data, HTTPStatus.OK, {}
         token = generate_token('1234', 'foo@user.com', 'foouser',
                                scope=[])
-        response = self.client.get('/TESTv1',headers={'Authorization':token})
+        response = self.client.get('/TESTv1', headers={'Authorization': token})
 
         self.assertEqual(response.status_code, HTTPStatus.OK)
         self.assertEqual(response.headers['Content-Type'],
@@ -118,12 +115,11 @@ class TestUIRoutes(TestCase):
     @mock.patch(f'{ui.__name__}.controllers.retrieve')
     def test_get_events(self, mock_retrieve: Any) -> None:
         """Endpoint /<string:arxiv_id_str>v<int:arxiv_ver>/log returns html about all events belonging to an id."""
-
-        return_data={'relations':[{'key1':'val1'},{'key1':'val2'}]}  
+        return_data = {'relations': [{'key1': 'val1'}, {'key1': 'val2'}]}
         mock_retrieve.return_value = return_data, HTTPStatus.OK, {}
         token = generate_token('1234', 'foo@user.com', 'foouser',
                                scope=[])
-        response = self.client.get('/TESTv1/log',headers={'Authorization':token})
+        response = self.client.get('/TESTv1/log', headers={'Authorization': token})
 
         self.assertEqual(response.status_code, HTTPStatus.OK)
         self.assertEqual(response.headers['Content-Type'],
